f7fc81e1a09bbd5da06c760072adbeb1141c7a00
CarlosBaldessari-Exe <marianobaldessari14@gmail.com>
2024-07-09 16:32:52 -0300

    Update app.js
    Curso 2 Alura Modificaciones Ultimos ejercicios

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 let titulo = document.querySelector('h1');
 
-titulo.innerHTML = 'Jogo do numero secreto';
-
-
-let paragrafoinnerHTML = 'Hora do desafio';
+titulo.innerHTML = 'Hora do desafio';
 
 function verificarChute() {
-  console.log('El boton fue cliqueado');
-  alert ('Eu Amo Js');
-  let numero1 = prompt ("Digite um numero entero");
-  let numero2 = prompt ("Digite um numero entero");
-  let soma = numero1 + numero2;
-  alert (soma);
-
-} function estados (){
-    const city = prompt ("Digite o nome do seu estado e da sua cidade");
-    if (city) {
-        alert (`Estive em ${city} e lembrei de voce`);
-    }
+  console.log('O botao foi clicado');
 }
+ function estados (){
+    const city = prompt ("Digite o nome do seu estado e da sua cidade");
+    let nomeDaCidade = prompt('Digite o nome de uma cidade do Brasil que você gosta muito:')
+    alert(`Estive em ${nomeDaCidade} e lembrei de você`)   //{FORMA CORRECTA}
+     //  if (city) {       Esta es mi forma 
+  //    alert (`Estive em ${city} e lembrei de voce`);
+    //}
+ }
+function exibirAlerta ()  {
+    alert ('Eu Amo Js');
+}
+function somandoDoisNumeros () {
+    let numero1 = prompt ("Digite um numero entero");
+    let numero2 = prompt ("Digite um numero entero");
+    let soma = numero1 + numero2;
+    alert (`${numero1} + ${numero2} = ${soma}`);
+    }
